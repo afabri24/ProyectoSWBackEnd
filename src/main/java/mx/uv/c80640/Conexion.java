30f007d368744e64b8f60568cb638a528d9aaa22
afabri24 <afabri24@gmail.com>
2022-12-11 14:52:44 -0600

    a13

--- src/main/java/mx/uv/c80640/Conexion.java
+++ src/main/java/mx/uv/c80640/Conexion.java
@@ -11,7 +11,7 @@ public class Conexion {
     // private static String url = processBuilder.environment().get("DB"); // "jdbc:mysql://db4free.net:3306/ejemplo";
     private static String driverName =  "com.mysql.cj.jdbc.Driver"; // "com.mysql.jdbc.Driver"; //
     // private static String username = processBuilder.environment().get("USERDB");
-    // private static String password = processBuilder.environment().get("PASSDB");;
+    // private static String password = processBuilder.environment().get("PASSDB");
     // variable de conexion
     private static String username ="admin_sw";
     private static String password ="admin_sw";
