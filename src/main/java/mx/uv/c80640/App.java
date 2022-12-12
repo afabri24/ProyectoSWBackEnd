0e8d83426381ddd0efea58d9c72847990b6b6845
afabri24 <afabri24@gmail.com>
2022-12-11 19:47:38 -0600

    a20

--- src/main/java/mx/uv/c80640/App.java
+++ src/main/java/mx/uv/c80640/App.java
@@ -19,18 +19,17 @@ public class App
         //System.out.println( "Hello World!" );
         options("/*", (request, response) -> {
             String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
-            System.out.println(accessControlRequestHeaders);
             if (accessControlRequestHeaders != null) {
                 response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
             }
             String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
-            System.out.println(accessControlRequestMethod);
             if (accessControlRequestMethod != null) {
                 response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
             }
             return "OK";
         });
-        before((req, res)-> res.header("Access-Control-Allow-Origin", "*"));
+        before((req, res) -> res.header("Access-Control-Allow-Origin", "*"));
+        before((req, res) -> res.type("application/Json"));
 
         get("/basedatos", (req, res)-> gson.toJson(DAO.listaMonumentos()));
         post("/datos", (req, res) -> {
