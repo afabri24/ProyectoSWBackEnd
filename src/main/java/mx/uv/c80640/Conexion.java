c3a3ac3a5372d01076101fe88c9e06a53d115748
afabri24 <afabri24@gmail.com>
2022-12-11 21:21:57 -0600

    a29

--- src/main/java/mx/uv/c80640/Conexion.java
+++ src/main/java/mx/uv/c80640/Conexion.java
@@ -7,14 +7,14 @@ import java.sql.SQLException;
 public class Conexion {
     private static ProcessBuilder processBuilder = new ProcessBuilder();
 
-    private static String url = "jdbc:mysql://db4free.net:3306/proyecto_sw";
-    // private static String url = processBuilder.environment().get("DB"); // "jdbc:mysql://db4free.net:3306/ejemplo";
+    // private static String url = "jdbc:mysql://db4free.net:3306/proyecto_sw";
+    private static String url = processBuilder.environment().get("DB"); // "jdbc:mysql://db4free.net:3306/ejemplo";
     private static String driverName =  "com.mysql.cj.jdbc.Driver"; // "com.mysql.jdbc.Driver"; //
-    // private static String username = processBuilder.environment().get("USERDB");
-    // private static String password = processBuilder.environment().get("PASSDB");
+    private static String username = processBuilder.environment().get("USERDB");
+    private static String password = processBuilder.environment().get("PASSDB");
     // variable de conexion
-    private static String username ="admin_sw";
-    private static String password ="admin_sw";
+    // private static String username ="admin_sw";
+    // private static String password ="admin_sw";
     private static Connection connection = null;
     public static Connection getConnection(){
         try {
