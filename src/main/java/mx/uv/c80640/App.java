eb4d5acf34d2ff377a8ae914b8369157b6b2791b
afabri24 <afabri24@gmail.com>
2022-12-11 20:31:51 -0600

    a24

--- src/main/java/mx/uv/c80640/App.java
+++ src/main/java/mx/uv/c80640/App.java
@@ -30,6 +30,20 @@ public class App
         });
         before((req, res) -> res.header("Access-Control-Allow-Origin", "*"));
         before((req, res) -> res.type("application/Json"));
+        
+        // options("/*", (request, response) -> {
+        //     String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+        //     if (accessControlRequestHeaders != null) {
+        //         response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+        //     }
+        //     String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+        //     if (accessControlRequestMethod != null) {
+        //         response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+        //     }
+        //     return "OK";
+        // });
+        // before((req, res) -> res.header("Access-Control-Allow-Origin", "*"));
+        // before((req, res) -> res.type("application/Json"));
 
         get("/basedatos", (req, res)-> gson.toJson(DAO.listaMonumentos()));
         post("/datos", (req, res) -> {
